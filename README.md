f17a843dc915471deda35a14b418fafece539b62
Sloane Tribble <SloaneTribble@gmail.com>
2022-06-28 13:52:54 -0700

    Begin setting up webpack

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # weather-mate
+
+This assignment from The Odin Project is designed to test students' recently acquired knowledge of promises, APIs and async/await.  
+
+Users may search for a city, state or country, and receive a relevant weather forecast.  
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  devtool: 'source-map',
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Weather Mate',
+    })
+  ],
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+};
